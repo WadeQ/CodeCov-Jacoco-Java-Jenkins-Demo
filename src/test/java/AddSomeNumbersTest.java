71bd4baebcdaf0d766641e30c874b5d625ccce02
supremos2366 <derrickwadek@gmail.com>
2021-05-12 17:28:06 +0300

    updated main

--- src/test/java/AddSomeNumbersTest.java
+++ src/test/java/AddSomeNumbersTest.java
@@ -7,7 +7,7 @@ class AddSomeNumbersTest {
     @Test
     void addTwoNumbers() {
         AddSomeNumbers addSomeNumbers = new AddSomeNumbers();
-        int result = addSomeNumbers.addTwoNumbers(40, 20);
-        assertEquals(60, result);
+        int result = addSomeNumbers.addTwoNumbers(45, 25);
+        assertEquals(70, result);
     }
 }
