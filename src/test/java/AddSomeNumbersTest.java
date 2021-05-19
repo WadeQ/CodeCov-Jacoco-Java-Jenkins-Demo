be2480d6caed94adf4d80bc521528bd0408ded18
supremos2366 <derrickwadek@gmail.com>
2021-05-19 23:21:29 +0300

    updated main

--- src/test/java/AddSomeNumbersTest.java
+++ src/test/java/AddSomeNumbersTest.java
@@ -1,12 +1,20 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class AddSomeNumbersTest {
 
     @Test
-    void addTwoNumbers() {
+    int addTwoNumbers() {
         AddSomeNumbers addSomeNumbers = new AddSomeNumbers();
         int result = addSomeNumbers.addTwoNumbers(50, 30);
         assertEquals(80, result);
+        return result;
+    }
+
+    @Test
+    void shouldPrintSumToConsole(){
+        AddSomeNumbers.main(new String[] {});
+        assertEquals(addTwoNumbers(), 80);
     }
 }
